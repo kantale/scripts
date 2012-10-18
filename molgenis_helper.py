c35c954d2508001ad4a463a11b383c203911ce7a
kantale <alexandros.kanterakis@gmail.com>
2012-10-18 15:47:38 +0200

    kits man kleei

--- molgenis_helper.py
+++ molgenis_helper.py
@@ -245,6 +245,7 @@ def compile_molgenis():
 
 	#After compile. Change persistence.xml.
 	#build/classes/META-INF/persistence.xml
+	# sed -i 's/validate/update/g' build/classes/META-INF/persistence.xml
 
 def start_molgenis(port = 8080):
 	if environment == 'vm':
