e3a8fd6b1449e6af8ce1b8654256062be82d2cf1
kantale <alexandros.kanterakis@gmail.com>
2012-10-19 12:31:01 +0200

    kits man kleei

--- molgenis_helper.py
+++ molgenis_helper.py
@@ -96,6 +96,9 @@ protocols_dir = os.path.join(scripts_dir, 'protocols')
 parameters = re.sub(r'clusterQueue,[\w\-]*,', r'clusterQueue,' + queue + ',', parameters)
 parameters = re.sub(r'scheduler,[\w\-]*,', r'scheduler,' + scheduler + ',', parameters)
 
+if scheduler == 'GRID':
+	parameters = re.sub(r'root,[\w\-]*,', r'root,' + '$WORKDIR' + ',', parameters)
+
 protocol_convertPedMapToTriTyper = {
 	"name" : "convertPedMapToTriTyper",
 	"content" : fetch_page_l('https://raw.github.com/freerkvandijk/molgenis_apps/master/modules/compute/protocols/imputation/impute2/protocols/convertPedMapToTriTyper.ftl') 
@@ -326,6 +329,39 @@ molgenis/lib/hibernate/javassist-3.12.0.GA.jar:molgenis/lib/hibernate/jta-1.1.ja
 org.molgenis.compute.test.util.WorksheetImporter -workflow_name %s -worksheet_file %s -McId %s """
 	command = command % (os.path.join(molgenis_apps_dir, '..'), os.path.split(workflow_fn)[1], worksheet_fn, run_name)
 	print "Running: " + command
+	os.system(command)
+
+def submit_script_to_grid():
+	command = """java -cp molgenis_apps/build/classes:molgenis/bin:\
+molgenis/lib/ant-1.8.1.jar:molgenis/lib/ant-apache-log4j.jar:molgenis/lib/aopalliance-1.0.jar:molgenis/lib/apache-poi-3.8.2:\
+molgenis/lib/arq.jar:molgenis/lib/asm-3.3.jar:molgenis/lib/axiom-api-1.2.7.jar:molgenis/lib/axiom-impl-1.2.7.jar:\
+molgenis/lib/bcprov-jdk15-1.43.jar:molgenis/lib/commons-codec-1.3.jar:molgenis/lib/commons-collections-3.2.1.jar:\
+molgenis/lib/commons-dbcp-1.2.1.jar:molgenis/lib/commons-fileupload-1.1.jar:molgenis/lib/commons-io-2.4.jar:\
+molgenis/lib/commons-lang-2.5.jar:molgenis/lib/commons-lang3-3.1.jar:molgenis/lib/commons-logging-1.1.1.jar:molgenis/lib/commons-pool-1.5.2.jar:\
+molgenis/lib/concurrent.jar:molgenis/lib/cxf-bundle-minimal-2.5.2.jar:molgenis/lib/d2rq-0.7.jar:molgenis/lib/d2r-server-0.7.jar:\
+molgenis/lib/dom4j-1.6.1.jar:molgenis/lib/FastInfoset-1.2.7.jar:molgenis/lib/freemarker.jar:molgenis/lib/ganymed-ssh2-build250.jar:\
+molgenis/lib/gson-2.2.1.jar:molgenis/lib/hsqldb.jar:molgenis/lib/icu4j_3_4.jar:molgenis/lib/iri.jar:molgenis/lib/jakarta-oro-2.0.8.jar:\
+molgenis/lib/javassist-3.12.0.GA.jar:molgenis/lib/javax.mail.jar:molgenis/lib/javax.servlet.jar:molgenis/lib/jaxb-impl-2.2.1.1.jar:\
+molgenis/lib/jaxb-xjc-2.2.1.1.jar:molgenis/lib/jaxen-1.1.jar:molgenis/lib/jdom-1.0.jar:molgenis/lib/jena.jar:molgenis/lib/jersey-json-1.1.5.jar:\
+molgenis/lib/jettison-1.2.jar:molgenis/lib/jetty-6.1.21.jar:molgenis/lib/jetty-html-6.1.21.jar:molgenis/lib/jetty-plus-6.1.21.jar:molgenis/lib/jetty-util-6.1.21.jar:\
+molgenis/lib/jopenid-1.07.jar:molgenis/lib/joseki.jar:molgenis/lib/jra-1.0-alpha-4.jar:molgenis/lib/js-1.7R1.jar:molgenis/lib/json.jar:molgenis/lib/jsr311-api-1.1.1.jar:\
+molgenis/lib/jta-1.1.jar:molgenis/lib/junit-4.8.2.jar:molgenis/lib/jxl.jar:molgenis/lib/log4j-1.2.15.jar:molgenis/lib/lucene-core-3.0.2.jar:molgenis/lib/mail.jar:\
+molgenis/lib/mysql-connector-java-5.1.2-beta-bin.jar:molgenis/lib/neethi-2.0.4.jar:molgenis/lib/oro-2.0.8.jar:molgenis/lib/postgresql-8.3-603.jdbc4.jar:\
+molgenis/lib/quartz-1.6.0.jar:molgenis/lib/selenium-server-standalone-2.19.0.jar:molgenis/lib/serializer-2.7.1.jar:molgenis/lib/slf4j-api-1.6.1.jar:\
+molgenis/lib/slf4j-log4j12-1.6.1.jar:molgenis/lib/smtp.jar:molgenis/lib/stax-utils-20060502.jar:molgenis/lib/tar.jar:molgenis/lib/testng-5.14.10.jar:\
+molgenis/lib/tjws-1.98.jar:molgenis/lib/validation-api-1.0.0.GA.jar:molgenis/lib/velocity-1.6.4.jar:molgenis/lib/wsdl4j-1.6.2.jar:molgenis/lib/wss4j-1.5.8.jar:\
+molgenis/lib/wstx-asl-3.2.8.jar:molgenis/lib/xalan-2.7.1.jar:molgenis/lib/xercesImpl.jar:molgenis/lib/xmlbeans-2.4.0.jar:molgenis/lib/xml-resolver-1.2.jar:\
+molgenis/lib/xmlrpc-client-3.1.3.jar:molgenis/lib/xmlrpc-common-3.1.3.jar:molgenis/lib/XmlSchema-1.4.7.jar:molgenis/lib/xmlsec-1.4.3.jar:molgenis/lib/xmltask.jar:\
+molgenis/lib/hibernate-validator-4.1.0.Final/hibernate-jpa-2.0-api-1.0.0.Final.jar:molgenis/lib/hibernate-validator-4.1.0.Final/hibernate-validator-4.1.0.Final.jar:\
+molgenis/lib/hibernate-validator-4.1.0.Final/log4j-1.2.14.jar:molgenis/lib/hibernate/antlr-2.7.6.jar:\
+molgenis/lib/hibernate-validator-4.1.0.Final/validation-api-1.0.0.GA.jar:\
+molgenis/lib/hibernate/commons-collections-3.1.jar:molgenis/lib/hibernate/dom4j-1.6.1.jar:molgenis/lib/hibernate/hibernate3.jar:\
+molgenis/lib/hibernate/hibernate-jpa-2.0-api-1.0.0.Final.jar:molgenis/lib/hibernate/hibernate-search-3.4.1.Final.jar:\
+molgenis/lib/hibernate/javassist-3.12.0.GA.jar:molgenis/lib/hibernate/jta-1.1.jar:slf4j-api-1.6.1.jar \
+org.molgenis.compute.test.RunPilotsOnBackEnd %s %s %s %s"""
+
+	command = command % ('ui.grid.sara.nl', 'byelas', 'str4ndb4l', 'grid')
+	print 'Running: ' + command
 	os.system(command)
 
 def import_workflow_to_molgenis(compile_molg = False):
