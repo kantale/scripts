40ad76bbc29f6e9d1aa6b9c13f877d24032749b5
kantale <alexandros.kanterakis@gmail.com>
2012-11-29 15:55:55 +0100

    kits man kleei

--- gridShare.py
+++ gridShare.py
@@ -88,7 +88,7 @@ def copy_files(cluster_root_dir, grid_root_dir):
 			#This is a directory
 			cluster_file_name_dir = os.path.split(cluster_file_name)[1]
 
-			command = 'srmmkdir %s' % os.path.join((constants[GRIDROOT]), 
+			command = 'srmmkdir %s' % os.path.join((constants[GRIDROOT]), cluster_file_name_dir)
 
 if __name__ == '__main__':
 
