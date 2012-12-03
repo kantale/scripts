ee7ccc6f52dd98d52da5a20d13f80f9aa4b71e6c
kantale <alexandros.kanterakis@gmail.com>
2012-12-03 12:11:32 +0100

    kits man kleei

--- gridShare.py
+++ gridShare.py
@@ -5,7 +5,6 @@ import urllib2
 
 constants = {
 	'GRIDROOT' : 'srm://srm.grid.sara.nl/pnfs/grid.sara.nl/data/bbmri.nl/RP2/resources/imputationReference/gonl_release3.1',
-	'CLUSTERROOT' : 'akanterakis@clustervp:/target/gpfs2/gcc/resources/imputationReference/gonl_release3.1',
 	'USERNAME' : 'akanterakis',
 	'REMOTEHOST' : 'clustervp',
 	'CLUSTERDIR' : '/target/gpfs2/gcc/resources/imputationReference/gonl_release3.1',
@@ -19,22 +18,6 @@ def list_files(dir_name):
 	os.system(command)
 	return [x.replace('\n', '') for x in open('file_list.txt').readlines()]
 
-def fetch_page(url):
-	print "Fetching: " + url
-	try:
-		fp = urllib2.urlopen(url)
-		ret = fp.read()
-		fp.close()
-	except urllib2.HTTPError:
-		print '   FAIL import'
-		ret = ''
-
-	return ret
-
-def fetch_page_l(url):
-	return lambda : fetch_page(url)
-
-gridShare_script_l = fetch_page_l('https://raw.github.com/georgebyelas/molgenis_apps/master/modules/compute/datatransfer/gridShare.sh')
 
 def get_param(name, arguments, default):
 
@@ -45,38 +28,6 @@ def get_param(name, arguments, default):
 
 	return default
 
-def fetch_gridShareScript():
-	content = gridShare_script_l()
-
-	content = re.sub(r'GRIDROOT="[\w\:\/\.]*"', r'GRIDROOT="%s"' % (constants['GRIDROOT']), content)
-	content = re.sub(r'CLUSTERROOT="[\w\:\/\.\@]*"', r'CLUSTERROOT="%s"' % (constants['CLUSTERROOT']), content)
-
-	open('gridShare.sh', 'w').write(content)
-
-def walk_dir(currentDir):
-    for root, dirs, files in os.walk(currentDir): # Walk directory tree
-		for dir_name in dirs:
-			yield 'dir', dir_name
-		for f in files:
-			yield 'file', os.path.join(root, f)
-
-def print_dir(dir_name):
-	walker = walk_dir(dir_name)
-
-	for x,y in walker:
-		print x,y
-
-def copy_to_grid(root_name, dir_name):
-	walker = walk_dir(dir_name)
-
-	for kind, name in walker:
-		if kind == 'dir':
-			command = ' '.join(['srmmkdir', os.path.join(root_name, name)])
-			yield command
-
-		if kind == 'file':
-			command = ' '.join(['getFile', os.path.join(root_name, name)])
-			yield command
 
 def all(a_list):
 	'''
@@ -130,25 +81,35 @@ def copy_files(cluster_root_dir, grid_root_dir, dummy = False, skip_dirs = []):
 			#This is a simple file
 			cluster_file_name_last = os.path.split(cluster_file_name)[1]
 
-			#Move the file to temporary local
-			command = 'scp %s@%s:%s %s' % (constants['USERNAME'], constants['REMOTEHOST'], cluster_file_name, os.path.join(constants['HOMEDIR'], constants['TMPDIR'], cluster_file_name_last))
-			exec_command(command, dummy)
-
 			grid_file_name_dir = os.path.join(grid_root_dir, cluster_file_name_last)
+			#Check if the file already exists on the grid:
+			command = 'srmls %s' % (grid_file_name_dir)
+			print command
+			returned = os.system(command)
+			if returned:
 
-			#Copy file to grid
-			command = 'srmcp -server_mode=passive file:///$HOME/%s %s' % (os.path.join(constants['TMPDIR'], cluster_file_name_last), grid_file_name_dir)
-			exec_command(command, dummy)
+				#Move the file to temporary local
+				command = 'scp %s@%s:%s %s' % (constants['USERNAME'], constants['REMOTEHOST'], cluster_file_name, os.path.join(constants['HOMEDIR'], constants['TMPDIR'], cluster_file_name_last))
+				exec_command(command, dummy)
 
-			#Remove the file from temporary local
-			command = 'rm %s' % (os.path.join(constants['HOMEDIR'], constants['TMPDIR'], cluster_file_name_last))
-			exec_command(command, dummy)
+				#Copy file to grid
+				command = 'srmcp -server_mode=passive file:///$HOME/%s %s' % (os.path.join(constants['TMPDIR'], cluster_file_name_last), grid_file_name_dir)
+				exec_command(command, dummy)
+
+				#Remove the file from temporary local
+				command = 'rm %s' % (os.path.join(constants['HOMEDIR'], constants['TMPDIR'], cluster_file_name_last))
+				exec_command(command, dummy)
+			else:
+				print 'File: ', grid_file_name_dir, 'already exists on the grid. Skip copying'
 
 if __name__ == '__main__':
 
 	#Example: python gridShare.py dummy=True skip=gonl_release3.1/jobs,gonl_release3.1/tmp 
  	dummy = eval(get_param('dummy', sys.argv, 'False'))
  	skip = get_param('skip', sys.argv, None)
+
+ 	for x in ['GRIDROOT', 'USERNAME', 'REMOTEHOST', 'CLUSTERDIR', 'TMPDIR', 'HOMEDIR']:
+ 		constants[x] = get_param(x, sys.argv, constants[x])
 
  	skip_dirs = []
  	if skip:
